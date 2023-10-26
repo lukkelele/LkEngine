909f4c6b86b634b4f97b189c5b769f1708de4ec1
lukkelele <encryptedluke@protonmail.com>
2023-10-26 19:54:56 +0200

    Renderer2D::DrawQuad now works somewhat

--- LkApplication/src/TestLayer.cpp
+++ LkApplication/src/TestLayer.cpp
@@ -37,7 +37,7 @@ void TestLayer::OnAttach()
     //EntityFactory::CreateRectangle(*m_Scene, { 20, 20 },   { 140.0f, 140.0f });
     //EntityFactory::CreateRectangle(*m_Scene, { 100, 100 }, { 400, 400.0f });
     EntityFactory::CreateRectangle(*m_Scene, { 500, 500 }, { 840, 840 });
-    EntityFactory::CreateRectangle(*m_Scene, { 0, 0}, { 240, 240 });
+    //EntityFactory::CreateRectangle(*m_Scene, { 0, 0}, { 240, 240 });
 
     m_Renderer2D = std::make_shared<Renderer2D>();
     m_Renderer2D->Init();
@@ -54,10 +54,13 @@ void TestLayer::OnUpdate(float ts)
     Physics2D::Raycast(m_Scene, mouse_pos, mouse_pos);
 
     auto& cam = *m_Scene->GetActiveCamera();
-
-    //m_Renderer2D->BeginScene(cam);
-    //m_Renderer2D->DrawQuad({ -0.50f, 0.0f }, { 0.4f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-    //m_Renderer2D->EndScene();
+    glm::vec2 camPos = cam.GetPos();
+    m_Renderer2D->BeginScene(cam);
+    //m_Renderer2D->DrawQuad({ -0.50f + camPos.x, 0.0f + camPos.y }, { 0.1f, 0.1f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+    //m_Renderer2D->DrawQuad({ -0.50f, 0.50f }, { 0.1f, 0.1f }, { 0.5f, 0.2f, 0.3f, 1.0f });
+    m_Renderer2D->DrawQuad({ 50.0f, 50.0f }, { 0.50f, 1.00f }, { 0.1f, 0.8f, 0.5f, 1.0f });
+    //m_Renderer2D->DrawQuad({ 0.0f, 0.0f }, { 100.0f, 100.0f }, { 0.1f, 0.8f, 0.5f, 1.0f });
+    m_Renderer2D->EndScene();
 }
 
 void TestLayer::OnImGuiRender()
