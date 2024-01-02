a55a4fc81a9ef445511376bf65a45e12c90806c7
lukkelele <{ID}+{username}@users.noreply.github.com>
2024-01-02 22:16:28 +0100

    Remove glsl version from initializing function and make the getters for width, height and viewport width and height inline

--- LkEngine/src/LkEngine/Core/Window.h
+++ LkEngine/src/LkEngine/Core/Window.h
@@ -22,16 +22,16 @@ namespace LkEngine {
         static s_ptr<Window> Create(const ApplicationSpecification& specification);
         static s_ptr<Window> Create(const char* title, uint32_t width, uint32_t height);
 
-        virtual void Init(const std::string& glslVersion = "#version 450") = 0;
+        virtual void Init(const std::string& shaderVersion) = 0;
         virtual void SwapBuffers() = 0;
         virtual void Exit() = 0;
         virtual void SetVSync(bool enabled) = 0;
         virtual GLFWwindow* GetGlfwWindow() const = 0;
         virtual s_ptr<GraphicsContext> GetContext() const = 0;
-        virtual uint16_t GetWidth() const = 0; 
-        virtual uint16_t GetHeight() const = 0; 
-        virtual uint16_t GetViewportWidth() const = 0; 
-        virtual uint16_t GetViewportHeight() const = 0;
+        inline virtual uint16_t GetWidth() const = 0; 
+        inline virtual uint16_t GetHeight() const = 0; 
+        inline virtual uint16_t GetViewportWidth() const = 0; 
+        inline virtual uint16_t GetViewportHeight() const = 0;
         virtual void SetWidth(uint16_t width) = 0;
         virtual void SetHeight(uint16_t height) = 0; 
         virtual void SetSize(const glm::vec2& size) = 0;
@@ -53,7 +53,6 @@ namespace LkEngine {
         virtual void SetScalers(const glm::vec2& scalers) = 0;
 
     protected:
-        inline static bool GLFW_Initialized = false;
         inline static Window* m_Instance = nullptr;
     };
 
