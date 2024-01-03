dce3939af84ba71215ddbc8ac743ccd3f64230b0
lukkelele <{ID}+{username}@users.noreply.github.com>
2024-01-03 16:18:36 +0100

    Add member 'm_Offset' and setters and getters for it. Will get replaced by the use of m_Origin later but I just went with this for now

--- LkEngine/src/LkEngine/Scene/SceneCamera.cpp
+++ LkEngine/src/LkEngine/Scene/SceneCamera.cpp
@@ -16,7 +16,6 @@ namespace LkEngine {
 	void SceneCamera::SetOrthographic(float width, float height, float nearClip, float farClip)
 	{
 		m_ProjectionType = ProjectionType::Orthographic;
-		//m_OrthographicSize = size;
 		m_OrthographicNear = nearClip;
 		m_OrthographicFar = farClip;
 	}
@@ -51,33 +50,30 @@ namespace LkEngine {
 			if (Keyboard::IsKeyPressed(Key::W))
 			{
 				m_Pos += glm::vec3(0, 1, 0) * ts * m_TravelSpeed;
-				//printf("Key -> W: m_Pos == (%f, %f, %f)\n", m_Pos.x, m_Pos.y, m_Pos.z);
 			}
 			if (Keyboard::IsKeyPressed(Key::A))
 			{
 				m_Pos -= glm::vec3(1, 0, 0) * ts * m_TravelSpeed;
-				//printf("Key -> A: m_Pos == (%f, %f, %f)\n", m_Pos.x, m_Pos.y, m_Pos.z);
 			}
 			if (Keyboard::IsKeyPressed(Key::S))
 			{
 				m_Pos -= glm::vec3(0, 1, 0) * ts * m_TravelSpeed;
-				//printf("Key -> S: m_Pos == (%f, %f, %f)\n", m_Pos.x, m_Pos.y, m_Pos.z);
 			}
 			if (Keyboard::IsKeyPressed(Key::D))
 			{
 				m_Pos += glm::vec3(1, 0, 0) * ts * m_TravelSpeed;
-				//printf("Key -> D: m_Pos == (%f, %f, %f)\n", m_Pos.x, m_Pos.y, m_Pos.z);
 			}
 		}
 
-		//UpdateMousePosition();
 		// If the mouse has moved since last frame, update camera rotation
+		//UpdateMousePosition();
 		//UpdateProjection();
 		//UpdateView();
 		//UpdateViewProjection();
-		//SetViewportSize(m_ViewportWidth, m_ViewportHeight);
+
 		auto* window = Window::Get();
 		SetViewportSize(window->GetViewportWidth(), window->GetViewportHeight());
+
 		m_InverseViewMatrix = glm::inverse(m_ViewMatrix);
 		m_InverseProjectionMatrix = glm::inverse(m_ProjectionMatrix);
 		m_InverseViewProjectionMatrix = m_InverseProjectionMatrix * m_InverseViewMatrix;
@@ -88,20 +84,23 @@ namespace LkEngine {
 	{
 		float left, right, bottom, top;
 		float width, height;
+
 		auto window = Application::Get()->GetWindow();
 		width = window->GetWidth();
 		height = window->GetWidth();
+
 		left = -width / 2.0f;
 		right = width / 2.0f;
 		bottom = -height / 2.0f;
 		top = height / 2.0f;
+
 		m_ProjectionMatrix = glm::ortho(left, right, bottom, top);
 	}
 
 	// FIXME: ortographic here, should be able to switch 
 	void SceneCamera::UpdateView()
 	{
-		glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_Pos) *
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), { m_Pos.x + m_Offset.x, m_Pos.y + m_Offset.y, m_Pos.z + m_Offset.z }) *
 			glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0, 0, 1));
 
 		m_ViewMatrix = glm::inverse(transform);
@@ -111,6 +110,25 @@ namespace LkEngine {
 	void SceneCamera::SetPos(const glm::vec3& pos)
 	{
 		m_Pos = pos;
+	}
+
+	void SceneCamera::SetOffset(const glm::vec2& offset)
+	{
+		SetOffset({ offset.x, offset.y, 0.0f });
+	}
+
+	void SceneCamera::SetOffset(const glm::vec3& offset)
+	{
+		m_Offset = offset;
+	}
+
+	glm::vec3 SceneCamera::GetPosWithOffset()
+	{
+		return { 
+			m_Pos.x + m_Offset.x, 
+			m_Pos.y + m_Offset.y, 
+			m_Pos.z + m_Offset.z
+		};
 	}
 
 
--- LkEngine/src/LkEngine/Scene/SceneCamera.h
+++ LkEngine/src/LkEngine/Scene/SceneCamera.h
@@ -19,12 +19,14 @@ namespace LkEngine {
 		void SetOrthographic(float width, float height, float nearClip = -1.0f, float farClip = 1.0f);
 		void SetViewportSize(uint32_t width, uint32_t height);
 
-		void Update(float ts);
+		void Update(float ts) override;
 		void UpdateView();
 		void UpdateProjection();
 		void UpdateViewProjection();
 
 		glm::vec3& GetPos() { return m_Pos; }
+		glm::vec3 GetPos() const { return m_Pos; }
+		glm::vec3 GetPosWithOffset();
 	    glm::mat4 GetView() const { return m_ViewMatrix; }
 	    glm::mat4 GetProjection() const { return m_ProjectionMatrix; }
 	    glm::mat4 GetViewProjection() const { return m_ViewProjectionMatrix; }
@@ -32,6 +34,11 @@ namespace LkEngine {
 		glm::mat4& GetInverseView() { return m_InverseViewMatrix; }
 		glm::mat4& GetInverseProjection() { return m_InverseProjectionMatrix; }
 		void SetPos(const glm::vec3& pos);
+
+		void SetOffset(const glm::vec2& offset);
+		void SetOffset(const glm::vec3& offset);
+		glm::vec3 GetOffset() const { return m_Offset; }
+		glm::vec3& GetOffset() { return m_Offset; }
 
 	private:
 		ProjectionType m_ProjectionType = ProjectionType::Perspective;
@@ -44,6 +51,7 @@ namespace LkEngine {
 		glm::vec3 m_Pos = { 0.0f, 0.0f, 0.0f };
 		glm::vec3 m_PosDelta = { 0.0f, 0.0f, 0.0f };
 		glm::vec3 m_Origin = { 0.0f, 0.0f, 0.0f };
+		glm::vec3 m_Offset = { 0.0f, 0.0f, 0.0f };
 		glm::vec2 m_MouseDelta = { 0.0f, 0.0f };
 		glm::vec2 m_MousePos = { 0.0f, 0.0f };
 		glm::vec2 m_InitialMousePos{};
