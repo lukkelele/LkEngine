645dae2d22859366bcb1948a102f8d17f1991440
lukkelele <{ID}+{username}@users.noreply.github.com>
2024-01-20 00:28:17 +0100

    Add VulkanImage, implementing Image

--- LkEngine/src/LkEngine/Platform/Vulkan/VulkanImage.cpp
+++ LkEngine/src/LkEngine/Platform/Vulkan/VulkanImage.cpp
@@ -1,6 +1,8 @@
 #include "LKpch.h"
 #include "VulkanImage.h"
 
+#include "LkEngine/Renderer/Renderer.h"
+
 
 namespace LkEngine {
 
@@ -12,13 +14,12 @@ namespace LkEngine {
 		if (data)
 		{
             uint32_t memorySize = Image::GetMemorySize(spec.Format, spec.Width, spec.Height);
-			LOG_DEBUG("VulkanImage::Memory size: {}    texture: {}", memorySize, spec.Name);
+			LK_CORE_DEBUG("VulkanImage::Memory size: {}    texture: {}", memorySize, spec.Name);
 			m_ImageData = Buffer::Copy(data, memorySize);
 
 			// Texture Wrap
 			if (m_Specification.Wrap == TextureWrap::Clamp)
 			{
-
 			}
 			else if (m_Specification.Wrap == TextureWrap::Repeat)
 			{
@@ -81,6 +82,19 @@ namespace LkEngine {
         }
     }
 
+    void VulkanImage::RT_Invalidate()
+    {
+		if (m_RendererID)
+		{
+            Release();
+		}
+        uint32_t mipCount = CalculateMipCount(m_Width, m_Height);
+
+        if (m_ImageData)
+        {
+        }
+    }
+
     void VulkanImage::AllocateMemory(uint64_t size)
     {
     }
@@ -93,6 +107,100 @@ namespace LkEngine {
         }
         m_ImageData.Release();
     }
+
+    void VulkanImage::UpdateDescriptor()
+    {
+		if (m_Specification.Format == ImageFormat::DEPTH24STENCIL8 || m_Specification.Format == ImageFormat::DEPTH32F || m_Specification.Format == ImageFormat::DEPTH32FSTENCIL8UINT)
+			m_DescriptorImageInfo.imageLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL;
+		else if (m_Specification.Usage == ImageUsage::Storage)
+			m_DescriptorImageInfo.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+		else
+			m_DescriptorImageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+
+		if (m_Specification.Usage == ImageUsage::Storage)
+			m_DescriptorImageInfo.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+		else if (m_Specification.Usage == ImageUsage::HostRead)
+			m_DescriptorImageInfo.imageLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+
+		m_DescriptorImageInfo.imageView = m_Info.ImageView;
+		m_DescriptorImageInfo.sampler = m_Info.Sampler;
+    }
+
+	void VulkanImage::CreatePerLayerImageViews()
+	{
+		Ref<VulkanImage> instance = this;
+		Renderer::Submit([instance]() mutable
+		{
+			instance->RT_CreatePerLayerImageViews();
+		});
+	}
+
+	void VulkanImage::RT_CreatePerLayerImageViews()
+	{
+		LK_CORE_ASSERT(m_Specification.Layers > 1);
+
+		VkDevice device = VulkanContext::GetCurrentDevice()->GetVulkanDevice();
+
+		VkImageAspectFlags aspectMask = Image::IsDepthFormat(m_Specification.Format) ? VK_IMAGE_ASPECT_DEPTH_BIT : VK_IMAGE_ASPECT_COLOR_BIT;
+		if (m_Specification.Format == ImageFormat::DEPTH24STENCIL8)
+			aspectMask |= VK_IMAGE_ASPECT_STENCIL_BIT;
+
+		const VkFormat vulkanFormat = VKUtils::VulkanImageFormat(m_Specification.Format);
+
+		m_PerLayerImageViews.resize(m_Specification.Layers);
+		for (uint32_t layer = 0; layer < m_Specification.Layers; layer++)
+		{
+			VkImageViewCreateInfo imageViewCreateInfo = {};
+			imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			imageViewCreateInfo.format = vulkanFormat;
+			imageViewCreateInfo.flags = 0;
+			imageViewCreateInfo.subresourceRange = {};
+			imageViewCreateInfo.subresourceRange.aspectMask = aspectMask;
+			imageViewCreateInfo.subresourceRange.baseMipLevel = 0;
+			imageViewCreateInfo.subresourceRange.levelCount = m_Specification.Mips;
+			imageViewCreateInfo.subresourceRange.baseArrayLayer = layer;
+			imageViewCreateInfo.subresourceRange.layerCount = 1;
+			imageViewCreateInfo.image = m_Info.Image;
+			VK_CHECK_RESULT(vkCreateImageView(device, &imageViewCreateInfo, nullptr, &m_PerLayerImageViews[layer]));
+			VKUtils::SetDebugUtilsObjectName(device, VK_OBJECT_TYPE_IMAGE_VIEW, fmt::format("{} image view layer: {}", m_Specification.DebugName, layer), m_PerLayerImageViews[layer]);
+		}
+	}
+
+	void VulkanImage::RT_CreatePerSpecificLayerImageViews(const std::vector<uint32_t>& layerIndices)
+	{
+		LK_CORE_ASSERT(m_Specification.Layers > 1);
+
+		VkDevice device = VulkanContext::GetCurrentDevice()->GetVulkanDevice();
+
+		VkImageAspectFlags aspectMask = Image::IsDepthFormat(m_Specification.Format) ? VK_IMAGE_ASPECT_DEPTH_BIT : VK_IMAGE_ASPECT_COLOR_BIT;
+		if (m_Specification.Format == ImageFormat::DEPTH24STENCIL8)
+			aspectMask |= VK_IMAGE_ASPECT_STENCIL_BIT;
+
+		const VkFormat vulkanFormat = VKUtils::VulkanImageFormat(m_Specification.Format);
+
+		if (m_PerLayerImageViews.empty())
+			m_PerLayerImageViews.resize(m_Specification.Layers);
+
+		for (uint32_t layer : layerIndices)
+		{
+			VkImageViewCreateInfo imageViewCreateInfo = {};
+			imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			imageViewCreateInfo.format = vulkanFormat;
+			imageViewCreateInfo.flags = 0;
+			imageViewCreateInfo.subresourceRange = {};
+			imageViewCreateInfo.subresourceRange.aspectMask = aspectMask;
+			imageViewCreateInfo.subresourceRange.baseMipLevel = 0;
+			imageViewCreateInfo.subresourceRange.levelCount = m_Specification.Mips;
+			imageViewCreateInfo.subresourceRange.baseArrayLayer = layer;
+			imageViewCreateInfo.subresourceRange.layerCount = 1;
+			imageViewCreateInfo.image = m_Info.Image;
+			VK_CHECK_RESULT(vkCreateImageView(device, &imageViewCreateInfo, nullptr, &m_PerLayerImageViews[layer]));
+			VKUtils::SetDebugUtilsObjectName(device, VK_OBJECT_TYPE_IMAGE_VIEW, fmt::format("{} image view layer: {}", m_Specification.DebugName, layer), m_PerLayerImageViews[layer]);
+		}
+
+	}
 
 }
 
--- LkEngine/src/LkEngine/Platform/Vulkan/VulkanImage.h
+++ LkEngine/src/LkEngine/Platform/Vulkan/VulkanImage.h
@@ -2,8 +2,22 @@
 
 #include "LkEngine/Renderer/Image.h"
 
+#include "VulkanDescriptorSetManager.h"
+#include "VulkanMemoryAllocator/vk_mem_alloc.h"
+#include "VulkanResourceDescriptorInfo.h"
+
+#include "VulkanContext.h"
+
 
 namespace LkEngine {
+
+	struct VulkanImageInfo
+	{
+		VkImage Image = nullptr;
+		VkImageView ImageView = nullptr;
+		VkSampler Sampler = nullptr;
+		VmaAllocation MemoryAlloc = nullptr;
+	};
 
     class VulkanImage : public Image
     {
@@ -12,32 +26,59 @@ namespace LkEngine {
 		VulkanImage(ImageSpecification spec, const void* data = nullptr);
 		~VulkanImage();
 
-		void Invalidate();
-		void Resize(uint32_t width, uint32_t height);
-		void SetData(const void* data);
+		void Invalidate() override;
+		void RT_Invalidate() override;
+
+		void Resize(uint32_t width, uint32_t height) override;
+		void SetData(const void* data) override;
 
 		RendererID& GetRendererID() { return m_RendererID; }
 		RendererID GetRendererID() const { return m_RendererID; }
 
-		Buffer GetBuffer() const { return m_ImageData; }
-		Buffer& GetBuffer() { return m_ImageData; }	
-		uint32_t GetWidth() const { return m_Specification.Width; }
-		uint32_t GetHeight() const { return m_Specification.Height; }
-		int64_t GetImageFormat(ImageFormat fmt);
-		const ImageSpecification GetImageSpecification() const { return m_Specification; }
+		Buffer GetBuffer() const override { return m_ImageData; }
+		Buffer& GetBuffer() override { return m_ImageData; }	
 
-	private:
+		uint32_t GetWidth() const override { return m_Specification.Width; }
+		uint32_t GetHeight() const override { return m_Specification.Height; }
+
+		const ImageSpecification GetSpecification() const { return m_Specification; }
+		ImageSpecification& GetSpecification() { return m_Specification; }
+
+		int64_t GetImageFormat(ImageFormat fmt) override;
+		VulkanImageInfo GetImageInfo() const { return m_Info; }
+		VulkanImageInfo& GetImageInfo() { return m_Info; }
+
+		void CreatePerLayerImageViews();
+		void RT_CreatePerLayerImageViews();
+		void RT_CreatePerSpecificLayerImageViews(const std::vector<uint32_t>& layerIndices);
+
+		virtual VkImageView GetLayerImageView(uint32_t layer)
+		{
+			LK_CORE_ASSERT(layer < m_PerLayerImageViews.size());
+			return m_PerLayerImageViews[layer];
+		}
+
+		virtual ResourceDescriptorInfo GetDescriptorInfo() const { return (ResourceDescriptorInfo)&m_DescriptorImageInfo; }
+		VkDescriptorImageInfo& GetDescriptorInfoVulkan() const { return *(VkDescriptorImageInfo*)GetDescriptorInfo(); }
+
 		void Release();
 		void AllocateMemory(uint64_t size);
+
+		void UpdateDescriptor();
 
 	private:
 		RendererID m_RendererID = 0;
 		Buffer m_ImageData;
 		uint32_t m_Width, m_Height;
+
+		VulkanImageInfo m_Info;
 		ImageSpecification m_Specification;
+		VkDescriptorImageInfo m_DescriptorImageInfo = {};
+		std::vector<VkImageView> m_PerLayerImageViews;
+
+		VkDeviceSize m_GPUAllocationSize;
 
 		std::string m_FilePath;
-
 		glm::vec4 m_TintColor = Color::RGBA::Transparent;
 	};
 
