4260ca01a96b7740b35bde8bdbe73bf056de6549
lukkelele <{ID}+{username}@users.noreply.github.com>
2024-01-20 00:14:46 +0100

    Update RendererAPI to use Ref's and move RendererAPIType to Base.h

--- LkEngine/src/LkEngine/Renderer/RendererAPI.cpp
+++ LkEngine/src/LkEngine/Renderer/RendererAPI.cpp
@@ -1,23 +1,20 @@
 #include "LKpch.h"
 #include "RendererAPI.h"
 
-
-#ifdef LK_RENDERER_API_VULKAN 
-    #include "LkEngine/Platform/Vulkan/VulkanRenderer.h"
-#elif defined(LK_RENDERER_API_OPENGL)
-    #include "LkEngine/Platform/OpenGl/OpenGLRenderer.h"
-#endif
+#include "LkEngine/Platform/Vulkan/VulkanRenderer.h"
+#include "LkEngine/Platform/OpenGl/OpenGLRenderer.h"
 
 
 namespace LkEngine {
 
-    s_ptr<RendererAPI> RendererAPI::Create()
+    Ref<RendererAPI> RendererAPI::Create()
     {
-    #ifdef LK_RENDERER_API_VULKAN 
-        return std::make_shared<VulkanRenderer>();
-    #elif defined(LK_RENDERER_API_OPENGL)
-        return std::make_shared<OpenGLRenderer>();
-    #endif
+        switch (RendererAPI::Current())
+        {
+            case RendererAPIType::Vulkan: return Ref<VulkanRenderer>::Create();
+            case RendererAPIType::OpenGL: return Ref<OpenGLRenderer>::Create();
+        }
+        LK_CORE_ASSERT(false, "No Render API detected");
     }
 
 }
--- LkEngine/src/LkEngine/Renderer/RendererAPI.h
+++ LkEngine/src/LkEngine/Renderer/RendererAPI.h
@@ -1,62 +1,78 @@
 #pragma once
 
-#include "Renderer2D.h"
+#include "SwapChain.h"
+#include "RenderPass.h"
+#include "RenderCommandBuffer.h"
+#include "RenderCommandQueue.h"
 #include "VertexBuffer.h"
-#include "TextureLibrary.h"
+#include "IndexBuffer.h"
+#include "GraphicsContext.h"
 #include "Shader.h"
+#include "UniformBuffer.h"
+#include "TextureLibrary.h"
+#include "MaterialLibrary.h"
+
+#include "RendererCapabilities.h"
 
 #include "LkEngine/Scene/Components.h"
+
+#include "Renderer2DAPI.h"
 
 
 namespace LkEngine {
 
-	enum class RendererAPIType
-	{
-		None,
-		OpenGL,
-		Vulkan
-	};
-
+	// TODO: Remove, use Topology instead
 	enum class RendererDrawMode 
 	{ 
 		Lines, 
 		Triangles 
 	};
 
-    class RendererAPI
+    class RendererAPI : public RefCounted
     {
 	public:
 		virtual void Init() = 0;
 		virtual void Shutdown() = 0;
 
-		static s_ptr<RendererAPI> Create();
-		static s_ptr<Renderer2D>& GetRenderer2D() { return m_Renderer2D; }
-		static void SetAPI(RendererAPIType api);
 		static RendererAPIType Current() { return m_CurrentRendererAPI; }
+		static Ref<RendererAPI> Create();
+		static void SetAPI(RendererAPIType api);
 
-		virtual void Clear() = 0;
+		virtual Ref<Renderer2DAPI> GetRenderer2D() = 0;
+		virtual Ref<Renderer2DAPI> GetRenderer2DAPI() = 0;
+
 		virtual void BeginFrame() = 0;
 		virtual void EndFrame() = 0;
+		virtual void Clear() = 0;
+
+		virtual void BeginRenderPass(Ref<RenderCommandBuffer> renderCommandBuffer, Ref<RenderPass> renderPass, bool explicitClear = false) = 0;
+		virtual void EndRenderPass(Ref<RenderCommandBuffer> renderCommandBuffer) = 0;
+		virtual void RenderGeometry(Ref<RenderCommandBuffer> renderCommandBuffer, Ref<Pipeline> pipeline, Ref<VertexBuffer> vertexBuffer, Ref<IndexBuffer> indexBuffer, const glm::mat4& transform, uint32_t indexCount = 0) = 0;
+		virtual void RenderGeometry(Ref<RenderCommandBuffer> renderCommandBuffer, Ref<Pipeline> pipeline, Ref<Shader> shader, Ref<VertexBuffer> vertexBuffer, Ref<IndexBuffer> indexBuffer, const glm::mat4& transform, uint32_t indexCount = 0) = 0;
+		virtual void RenderGeometry(Ref<RenderCommandBuffer> renderCommandBuffer, Ref<Pipeline> pipeline, Ref<Material> material, Ref<VertexBuffer> vertexBuffer, Ref<IndexBuffer> indexBuffer, const glm::mat4& transform, uint32_t indexCount = 0) = 0;
 
 		virtual void SetDrawMode(const RendererDrawMode& mode) = 0;
 		virtual void Draw(VertexBuffer& vb, const Shader& shader) = 0;
 		virtual void Draw(const VertexBuffer& vb, const IndexBuffer& ib, const Shader& shader) = 0;
+        virtual void DrawIndexed(uint64_t indexCount) = 0;
 
-		virtual void SubmitLine(const glm::vec2& p1, const glm::vec2& p2, const glm::vec4& color, uint64_t entityID = 0) = 0;
+		virtual void SubmitIndexed(unsigned int indexCount) = 0;
+
 		virtual void SubmitQuad(const glm::vec2& pos, const glm::vec2& size, const glm::vec4& color, uint64_t entityID = 0) = 0;
 		virtual void SubmitQuad(const glm::vec3& pos, const glm::vec2& size, const glm::vec4& color, uint64_t entityID = 0) = 0;
 		virtual void SubmitQuad(const glm::vec2& pos, const glm::vec2& size, const glm::vec4& color, float rotation, uint64_t entityID = 0) = 0;
 		virtual void SubmitQuad(const glm::vec3& pos, const glm::vec2& size, const glm::vec4& color, float rotation, uint64_t entityID = 0) = 0;
-		virtual void SubmitQuad(const glm::vec2& pos, const glm::vec2& size, s_ptr<Texture> texture, float rotation, uint64_t entityID = 0) = 0;
-		virtual void SubmitQuad(const glm::vec3& pos, const glm::vec2& size, s_ptr<Texture> texture, float rotation, uint64_t entityID = 0) = 0;
-		virtual void SubmitQuad(const glm::vec2& pos, const glm::vec2& size, s_ptr<Texture> texture, const glm::vec4& tintColor, float rotation, uint64_t entityID) = 0;
-		virtual void SubmitQuad(const glm::vec3& pos, const glm::vec2& size, s_ptr<Texture> texture, const glm::vec4& tintColor, float rotation, uint64_t entityID) = 0;
-		virtual void SubmitSprite(TransformComponent& tc, const glm::vec2& size, const glm::vec4 color, uint64_t entityID = 0) = 0;
-		virtual void SubmitIndexed(unsigned int indexCount) = 0;
+		virtual void SubmitQuad(const glm::vec2& pos, const glm::vec2& size, Ref<Texture> texture, float rotation, uint64_t entityID = 0) = 0;
+		virtual void SubmitQuad(const glm::vec3& pos, const glm::vec2& size, Ref<Texture> texture, float rotation, uint64_t entityID = 0) = 0;
+		virtual void SubmitQuad(const glm::vec2& pos, const glm::vec2& size, Ref<Texture> texture, const glm::vec4& tintColor, float rotation, uint64_t entityID) = 0;
+		virtual void SubmitQuad(const glm::vec3& pos, const glm::vec2& size, Ref<Texture> texture, const glm::vec4& tintColor, float rotation, uint64_t entityID) = 0;
+
+		virtual void SubmitLine(const glm::vec2& p1, const glm::vec2& p2, const glm::vec4& color, uint64_t entityID = 0) = 0;
+
+		virtual RendererCapabilities& GetCapabilities() = 0;
 		
 	protected:
 		inline static RendererAPIType m_CurrentRendererAPI = RendererAPIType::OpenGL;
-		inline static s_ptr<Renderer2D> m_Renderer2D = nullptr;
 
 		friend class Renderer;
 		friend class Editor;
