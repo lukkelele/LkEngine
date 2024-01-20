2fa2bcb16b154764efd6dbb2e102c7f2135cfde1
lukkelele <{ID}+{username}@users.noreply.github.com>
2024-01-20 12:59:30 +0100

    Clean up Color.h a bit, remove unnecesary includes

--- LkEngine/src/LkEngine/Renderer/Color.h
+++ LkEngine/src/LkEngine/Renderer/Color.h
@@ -1,34 +1,25 @@
 #pragma once
 
-#include <glm/glm.hpp>
-#include <random>
-#include <time.h>
+#include "LkEngine/Core/Math/Math.h"
 
 
-namespace LkEngine {
+namespace LkEngine::Color {
 
+    extern std::mt19937 RNG;
+    extern std::uniform_int_distribution<int> Dist;
 
-    namespace Color {
+    glm::vec4 Generate(float alpha = 1.0f);
+    void Randomize(glm::vec4& color, float alpha = 1.0f);
 
-        extern std::mt19937 RNG;
-        extern std::uniform_int_distribution<int> Dist;
-
-        glm::vec4 Generate(float alpha = 1.0f);
-        void Randomize(glm::vec4& color, float alpha = 1.0f);
-
-        namespace RGBA 
-        {
-            extern glm::vec4 Transparent;
-            extern glm::vec4 White;
-            extern glm::vec4 Black;
-            extern glm::vec4 Gray;
-            extern glm::vec4 Red;
-            extern glm::vec4 Green;
-            extern glm::vec4 Blue;
-        }
-
+    namespace RGBA 
+    {
+        extern glm::vec4 Transparent;
+        extern glm::vec4 White;
+        extern glm::vec4 Black;
+        extern glm::vec4 Gray;
+        extern glm::vec4 Red;
+        extern glm::vec4 Green;
+        extern glm::vec4 Blue;
     }
-
-
 
 }
