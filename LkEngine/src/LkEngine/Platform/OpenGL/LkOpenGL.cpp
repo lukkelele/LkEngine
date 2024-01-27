58de7d75bcfec7aa92e4a453a120875d79c252cf
lukkelele <{ID}+{username}@users.noreply.github.com>
2024-01-27 15:15:07 +0100

    Refactor raw openGL debug code to LkOpenGL instead of in OpenGLRenderer2D

--- LkEngine/src/LkEngine/Platform/OpenGL/LkOpenGL.cpp
+++ LkEngine/src/LkEngine/Platform/OpenGL/LkOpenGL.cpp
@@ -1,30 +1,424 @@
 #include "LKpch.h"
 #include "LkOpenGL.h"
 
+#include "LkEngine/Renderer/Renderer.h"
+#include "LkEngine/Renderer/Renderer2D.h"
+
+#include "OpenGLRenderer.h"
+#include "OpenGLRenderer2D.h"
+#include "OpenGLShader.h"
+
 
 namespace LkEngine {
 
-	void OpenGL_ClearError() 
-	{ 
-		while (glGetError() != GL_NO_ERROR); 
+	namespace GLUtils {
+
+		int GetOpenGLSourceBlendFunction(const SourceBlendFunction& srcFunc)
+		{
+			switch (srcFunc)
+			{
+				case SourceBlendFunction::Zero:  return GL_ZERO;
+				case SourceBlendFunction::One:   return GL_ONE;
+				case SourceBlendFunction::Alpha: return GL_ALPHA;
+				case SourceBlendFunction::Color: return GL_COLOR;
+				case SourceBlendFunction::One_Minus_DestinationAlpha: return GL_ONE_MINUS_DST_ALPHA;
+				default: throw std::runtime_error("Source blend function could not be retrieved correctly");
+			}
+		}
+	
+		int GetOpenGLDestinationBlendFunction(const DestinationBlendFunction& dstFunc)
+		{
+			switch (dstFunc)
+			{
+				case DestinationBlendFunction::Zero:  return GL_ZERO;
+				case DestinationBlendFunction::One:   return GL_ONE;
+				case DestinationBlendFunction::Alpha: return GL_ALPHA;
+				case DestinationBlendFunction::Color: return GL_COLOR;
+				case DestinationBlendFunction::One_Minus_SourceAlpha: return GL_ONE_MINUS_SRC_ALPHA;
+				default: throw std::runtime_error("Destination blend function could not be retrieved correctly");
+			}
+		}
+
+		const GLubyte* GetExtensions()
+		{
+			return glGetString(GL_EXTENSIONS);
+		}
+
+		void PrintOpenGLExtensions()
+		{
+			int n;
+			glGetIntegerv(GL_NUM_EXTENSIONS, &n);
+			for (int i = 0; i < n; i++)
+			{
+				const GLubyte* extension = glGetStringi(GL_EXTENSIONS, i);
+				LK_CORE_DEBUG_TAG("OpenGL Extension", "{}", extension);
+			}
+		}
+
+		GLenum OpenGLImageFormat(ImageFormat format)
+		{
+			switch (format)
+			{
+				case ImageFormat::RGB:     return GL_RGB;
+				case ImageFormat::RGBA:
+				case ImageFormat::RGBA8:
+				case ImageFormat::RGBA16F:
+				case ImageFormat::RGBA32F: return GL_RGBA;
+
+				case ImageFormat::SRGB:    return GL_SRGB;
+				case ImageFormat::SRGBA:   return GL_SRGB_ALPHA;
+			}
+			LK_CORE_ASSERT(false, "Unknown OpenGLImageFormat {}", Utils::ImageFormatToString(format));
+		}
+
+		GLenum OpenGLImageInternalFormat(ImageFormat format)
+		{
+			switch (format)
+			{
+				case ImageFormat::RGB:             return GL_RGB8;
+				case ImageFormat::RGBA:            return GL_RGBA8;
+				case ImageFormat::RGBA8:           return GL_RGBA8;
+				case ImageFormat::RGBA16F:         return GL_RGBA16F;
+				case ImageFormat::RGBA32F:         return GL_RGBA32F;
+				case ImageFormat::DEPTH24STENCIL8: return GL_DEPTH24_STENCIL8;
+				case ImageFormat::DEPTH32F:        return GL_DEPTH_COMPONENT32F;
+			}
+			LK_CORE_ASSERT(false, "Unknown OpenGLImageFormat {}", Utils::ImageFormatToString(format));
+		}
+
+		GLenum OpenGLFormatDataType(ImageFormat format)
+		{
+			switch (format)
+			{
+				case ImageFormat::RGB:
+				case ImageFormat::RGBA:    
+				case ImageFormat::RGBA8:   return GL_UNSIGNED_BYTE;
+				case ImageFormat::RGBA16F:
+				case ImageFormat::RGBA32F: return GL_FLOAT;
+			}
+			LK_CORE_ASSERT(false, "Unknown OpenGLFormatDataType {}", Utils::ImageFormatToString(format));
+		}
+
+		GLenum OpenGLSamplerWrap(TextureWrap wrap)
+		{
+			switch (wrap)
+			{
+				case TextureWrap::Clamp:   return GL_CLAMP_TO_EDGE;
+				case TextureWrap::Repeat:  return GL_REPEAT;
+			}
+			LK_CORE_ASSERT(false, "Unknown OpenGLSamplerWrap: {}", (int)wrap);
+		}
+
+		GLenum OpenGLSamplerFilter(TextureFilter filter, bool mipmap)
+		{
+			switch (filter)
+			{
+				case TextureFilter::Linear:  return mipmap ? GL_LINEAR_MIPMAP_LINEAR : GL_LINEAR;
+				case TextureFilter::Nearest: return mipmap ? GL_NEAREST_MIPMAP_NEAREST : GL_NEAREST;
+			}
+			LK_CORE_ASSERT(false, "Unknown OpenGLSamplerFilter: {}, mipmap: {}", (int)filter, mipmap);
+		}
+
+		GLenum ImageFormatToGLDataFormat(ImageFormat format)
+		{
+			switch (format)
+			{
+				case ImageFormat::RGBA:   
+				case ImageFormat::RGBA8:   
+				case ImageFormat::RGBA16F:   
+				case ImageFormat::RGBA32F:  return GL_RGBA;
+
+				case ImageFormat::RG8:     
+				case ImageFormat::RG16F:     
+				case ImageFormat::RG32F:    return GL_RG;
+
+				case ImageFormat::RGB:  
+				case ImageFormat::RGB8:     return GL_RGB;
+
+				case ImageFormat::RED8UI:
+				case ImageFormat::RED16UI:
+				case ImageFormat::RED32UI:  return GL_RED_INTEGER;
+				case ImageFormat::RED32F:   return GL_RED_INTEGER;
+
+			}
+			LK_CORE_ASSERT(false, "Unknown ImageFormat: {}", Utils::ImageFormatToString(format));
+		}
+
+		GLenum ImageFormatToGLInternalFormat(ImageFormat format)
+		{
+			switch (format)
+			{
+				case ImageFormat::RGB:      return GL_RGB32F;
+				case ImageFormat::RGB8:     return GL_RGB8;
+
+				case ImageFormat::RGBA:     return GL_RGBA32F;
+				case ImageFormat::RGBA8:    return GL_RGBA8;
+				case ImageFormat::RGBA16F:  return GL_RGBA16F;
+				case ImageFormat::RGBA32F:  return GL_RGBA32F;
+
+				case ImageFormat::RG16F:    return GL_RG16F;
+				case ImageFormat::RG32F:    return GL_RG32F;
+
+				case ImageFormat::RED8UI:   return GL_R8UI;
+				case ImageFormat::RED16UI:  return GL_R16UI;
+				case ImageFormat::RED32UI:  return GL_R32UI;
+				case ImageFormat::RED32F:   return GL_R32F;
+			}
+			LK_CORE_ASSERT(false, "Unknown internal ImageFormat: {}", Utils::ImageFormatToString(format));
+		}
+
+		GLenum TextureTarget(bool multisampled)
+		{
+			return multisampled ? GL_TEXTURE_2D_MULTISAMPLE : GL_TEXTURE_2D;
+		}
+
+		void CreateTextures(bool multisampled, uint32_t* outID, uint32_t count)
+		{
+			glCreateTextures(TextureTarget(multisampled), count, outID);
+		}
+
+		void BindTexture(bool multisampled, uint32_t id)
+		{
+			glBindTexture(TextureTarget(multisampled), id);
+		}
+
+		void AttachColorTexture(uint32_t id, int samples, GLenum internalFormat, GLenum format, uint32_t width, uint32_t height, int index)
+		{
+			bool multisampled = samples > 1;
+			if (multisampled)
+			{
+				glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples, internalFormat, width, height, GL_FALSE);
+			}
+			else
+			{
+				glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, nullptr);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			}
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + index, TextureTarget(multisampled), id, 0);
+		}
+
+		void AttachDepthTexture(uint32_t id, int samples, GLenum format, GLenum attachmentType, uint32_t width, uint32_t height)
+		{
+			bool multisampled = samples > 1;
+			if (multisampled)
+			{
+				glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples, format, width, height, GL_FALSE);
+			}
+			else
+			{
+				glTexStorage2D(GL_TEXTURE_2D, 1, format, width, height);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			}
+			glFramebufferTexture2D(GL_FRAMEBUFFER, attachmentType, TextureTarget(multisampled), id, 0);
+		}
+
+		GLenum FramebufferTextureFormatToGL(ImageFormat format)
+		{
+			switch (format)
+			{
+				case ImageFormat::RGBA8:            return GL_RGBA8;
+				case ImageFormat::RGBA16F:          return GL_RGBA16F;
+				case ImageFormat::RGBA32F:          return GL_RGBA32F;
+				case ImageFormat::RED8UI:      
+				case ImageFormat::RED8UN:      
+				case ImageFormat::RED16UI:       
+				case ImageFormat::RED32UI:          return GL_RED_INTEGER;
+				case ImageFormat::DEPTH24STENCIL8:  return GL_DEPTH24_STENCIL8;
+			}
+			LK_CORE_ASSERT(false, "FramebufferTextureFormatToGL: ImageFormat not recognized!");
+		}
+
+        std::string FramebufferTextureFormatToString(FramebufferTextureFormat format)
+		{
+			switch (format)
+			{
+				case FramebufferTextureFormat::RGBA8:           return "RGBA8";
+				case FramebufferTextureFormat::RED_INTEGER:     return "RED_INTEGER";
+				case FramebufferTextureFormat::DEPTH24STENCIL8: return "DEPTH24STENCIL8";
+				case FramebufferTextureFormat::None:            return "None";
+			}
+			LK_CORE_ASSERT(false, "Unknown FramebufferTextureFormat");
+		};
+
 	}
 
-    bool OpenGL_LogCall(const char* function, const char* file, int line)
+    //unsigned int FramebufferID;
+    unsigned int TextureColorBufferID, DepthTextureID;
+    unsigned int CubeTexture, FloorTexture;
+
+    unsigned int CubeVAO, CubeVBO;
+    unsigned int PlaneVAO, PlaneVBO;
+    unsigned int QuadVAO, QuadVBO;
+
+    void RenderMirrorTexture(const glm::mat4& view, const glm::mat4& proj)
     {
-        while (GLenum error = glGetError())
+		if (!DebugShader || !ScreenShader)
+		{
+			ScreenShader = Renderer::GetShaderLibrary()->Get("Renderer2D_Screen");
+			DebugShader  = Renderer::GetShaderLibrary()->Get("Renderer2D_Debug");
+			return;
+		}
+
+        Renderer2DAPI::Get().As<OpenGLRenderer2D>()->GetFramebuffer()->Bind();
+        glEnable(GL_DEPTH_TEST); // enable depth testing (is disabled for rendering screen-space quad)
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        DebugShader->Bind();
+        Model = glm::mat4(1.0f);
+        DebugShader->Set("View", view);
+        DebugShader->Set("Projection", proj);
+
+        // Cubes
         {
-            std::cout << "[OpenGL Error] (" << error << ")\nFUNCTION: " << function <<
-                "\nFILE: " << file << "\nLINE: " << line << std::endl;
-            return false;
+            glBindVertexArray(CubeVAO);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, CubeTexture);
+
+            // Cube 1
+            Model = glm::translate(Model, glm::vec3(-1.0f, 0.0f, -1.0f));
+            DebugShader->Set("Model", Model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+            // Cube 2
+            Model = glm::mat4(1.0f);
+            Model = glm::translate(Model, glm::vec3(2.0f, 0.0f, 0.0f));
+            DebugShader->Set("Model", Model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
         }
-        return true;
+        // Floor 
+        {
+            glBindVertexArray(PlaneVAO);
+            glBindTexture(GL_TEXTURE_2D, FloorTexture);
+
+            DebugShader->Set("Model", glm::mat4(1.0f));
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+        }
+        glBindVertexArray(0);
     }
 
-	const char* OpenGL_GetVersion()
-	{
-		char buf[120];
-		sprintf(buf, "%s", glGetString(GL_VERSION));
-		return buf;
-	}
+    void RenderScreenTexture(const glm::mat4& view, const glm::mat4& proj)
+    {
+		if (!DebugShader || !ScreenShader)
+		{
+			ScreenShader = Renderer::GetShaderLibrary()->Get("Renderer2D_Screen");
+			DebugShader  = Renderer::GetShaderLibrary()->Get("Renderer2D_Debug");
+			return;
+		}
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClearColor(0.1f, 0.2f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        Model = glm::mat4(1.0f);
+        DebugShader->Bind();
+        DebugShader->Set("View", view);
+        DebugShader->Set("Projection", proj);
+
+        // Cubes
+        {
+            // Cube 1
+            glBindVertexArray(CubeVAO);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, CubeTexture);
+            Model = glm::translate(Model, glm::vec3(-1.0f, 0.0f, -1.0f));
+            DebugShader->Set("Model", Model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+            // Cube 2
+            Model = glm::mat4(1.0f);
+            Model = glm::translate(Model, glm::vec3(2.0f, 0.0f, 0.0f));
+            DebugShader->Set("Model", Model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        // Floor
+        {
+            glBindVertexArray(PlaneVAO);
+            glBindTexture(GL_TEXTURE_2D, FloorTexture);
+            DebugShader->Set("Model", glm::mat4(1.0f));
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+            glBindVertexArray(0);
+        }
+
+        // Now draw the mirror quad with screen texture
+        glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
+        ScreenShader->Bind();
+        glBindVertexArray(QuadVAO);
+
+        //glBindTexture(GL_TEXTURE_2D, TextureColorBufferID);	// use the color attachment texture as the texture of the quad plane
+        unsigned int colorAttachment0 = Renderer2DAPI::Get().As<OpenGLRenderer2D>()->GetFramebuffer()->GetColorAttachmentRendererID(0);
+        glBindTexture(GL_TEXTURE_2D, colorAttachment0);	// use the color attachment texture as the texture of the quad plane
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        DebugShader->Unbind();
+        ScreenShader->Unbind();
+    }
+
+    void GenerateCubeVaoAndVbo(unsigned int& vao, unsigned int& vbo)
+    {
+        glGenVertexArrays(1, &vao);
+        glGenBuffers(1, &vbo);
+
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(Cube_Vertices), &Cube_Vertices, GL_STATIC_DRAW);
+
+        // Vertex indexing
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+
+        // Texture indexing
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    }
+
+    void GeneratePlaneVaoAndVbo(unsigned int& vao, unsigned int& vbo)
+    {
+        glGenVertexArrays(1, &vao);
+        glGenBuffers(1, &vbo);
+
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(Plane_Vertices), &Plane_Vertices, GL_STATIC_DRAW);
+
+        // Vertex indexing
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+
+        // Texture indexing
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    }
+
+    void GenerateScreenQuadVaoAndVbo(unsigned int& vao, unsigned int& vbo)
+    {
+        glGenVertexArrays(1, &vao);
+        glGenBuffers(1, &vbo);
+
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(Quad_Vertices), &Quad_Vertices, GL_STATIC_DRAW);
+
+        // Vertex indexing
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+
+        // Texture indexing
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    }
+
+    Ref<Shader> GetDebugShader() { return DebugShader; }
+    Ref<Shader> GetScreenShader() { return ScreenShader; }
 
 }
--- LkEngine/src/LkEngine/Platform/OpenGL/LkOpenGL.h
+++ LkEngine/src/LkEngine/Platform/OpenGL/LkOpenGL.h
@@ -1,14 +1,42 @@
 #pragma once
-#ifdef LK_RENDERER_API_OPENGL
 
 #include <glad/glad.h>
 
+#include "LkEngine/Renderer/BlendingSpecification.h"
+#include "LkEngine/Renderer/Image.h"
+
+#include "LkEngine/Utilities/StringUtils.h"
+#include "LkEngine/Utilities/MemoryUtils.h"
+
+#include "LkEngine/Renderer/Framebuffer.h"
+#include "LkEngine/Renderer/Shader.h"
+
+
 namespace LkEngine {
 
-    // OpenGL error handling functions
-    void OpenGL_ClearError();
-    bool OpenGL_LogCall(const char* function, const char* file, int line);
-    const char* OpenGL_GetVersion();
+	static void OpenGL_ClearError() 
+	{ 
+		while (glGetError() != GL_NO_ERROR); 
+	}
+
+    static bool OpenGL_LogCall(const char* function, const char* file, int line)
+    {
+        while (GLenum error = glGetError())
+        {
+            std::cout << "[OpenGL Error] (" << error << ")\nFUNCTION: " << function <<
+                "\nFILE: " << file << "\nLINE: " << line << std::endl;
+            return false;
+        }
+        return true;
+    }
+
+	static const char* OpenGL_GetVersion()
+	{
+		char buf[120];
+		sprintf(buf, "%s", glGetString(GL_VERSION));
+		return buf;
+	}
+
 
     // OpenGL call macro to use for invoking opengl functions, provides error handling
     #define GL_CALL(_FUNC) OpenGL_ClearError(); _FUNC; LK_ASSERT(OpenGL_LogCall(#_FUNC, __FILE__, __LINE__))
@@ -18,11 +46,164 @@ namespace LkEngine {
     constexpr const char* OpenGL_GLSL_33 = "#version 330";
     constexpr const char* OpenGL_GLSL_45 = "#version 450";
 
+    namespace GLUtils {
+
+        int GetOpenGLSourceBlendFunction(const SourceBlendFunction& srcFunc);
+        int GetOpenGLDestinationBlendFunction(const DestinationBlendFunction& dstFunc);
+
+        const GLubyte* GetExtensions();
+        void PrintOpenGLExtensions();
+
+        GLenum OpenGLFormatDataType(ImageFormat format);
+        GLenum OpenGLImageFormat(ImageFormat format);
+        GLenum OpenGLImageInternalFormat(ImageFormat format);
+        GLenum OpenGLSamplerWrap(TextureWrap wrap);
+        GLenum OpenGLSamplerFilter(TextureFilter filter, bool mipmap);
+        GLenum ImageFormatToGLDataFormat(ImageFormat format);
+        GLenum ImageFormatToGLInternalFormat(ImageFormat format);
+
+        void BindTexture(bool multisampled, uint32_t id);
+        void CreateTextures(bool multisampled, uint32_t* outID, uint32_t count);
+        GLenum TextureTarget(bool multisampled);
+        void AttachColorTexture(uint32_t id, int samples, GLenum internalFormat, GLenum format, uint32_t width, uint32_t height, int index);
+        void AttachDepthTexture(uint32_t id, int samples, GLenum format, GLenum attachmentType, uint32_t width, uint32_t height);
+        bool IsDepthFormat(ImageFormat format);
+        GLenum FramebufferTextureFormatToGL(ImageFormat format);
+        std::string FramebufferTextureFormatToString(FramebufferTextureFormat format);
+        std::string ImageFormatToString(ImageFormat format);
+
+    }
+
+
+    //=====================================================================
+    // Debugging
+    extern unsigned int CubeTexture, FloorTexture;
+
+    extern unsigned int CubeVAO, CubeVBO;
+    extern unsigned int PlaneVAO, PlaneVBO;
+    extern unsigned int QuadVAO, QuadVBO;
+
+    inline static Ref<Shader> ScreenShader = nullptr;
+    inline static Ref<Shader> DebugShader = nullptr;
+
+    inline static glm::mat4 Model = glm::mat4(1.0f);
+    inline static glm::mat4 View = glm::mat4(1.0f);
+    inline static glm::mat4 Projection = glm::mat4(1.0f);
+
+    static float Cube_Vertices[] = {
+        // Positions         // Texture Coords
+        -0.5f, -0.5f, -0.5f,    0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,    1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,    0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,    0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,    0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,    1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,    1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,    1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,    0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,    0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,    0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,    0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,    1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,    1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,    1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,    0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,    0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,    0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,    0.0f, 1.0f
+    };
+    static float Plane_Vertices[] = {
+        // Positions           // Texture Coords 
+         5.0f, -0.5f,  5.0f,   2.0f, 0.0f,
+        -5.0f, -0.5f,  5.0f,   0.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f,   0.0f, 2.0f,
+    
+         5.0f, -0.5f,  5.0f,   2.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f,   0.0f, 2.0f,
+         5.0f, -0.5f, -5.0f,   2.0f, 2.0f
+    };
+    static float Quad_Vertices[] = { 
+        // positions   // texCoords
+        -0.3f,  1.0f,  0.0f, 1.0f,
+        -0.3f,  0.7f,  0.0f, 0.0f,
+         0.3f,  0.7f,  1.0f, 0.0f,
+    
+        -0.3f,  1.0f,  0.0f, 1.0f,
+         0.3f,  0.7f,  1.0f, 0.0f,
+         0.3f,  1.0f,  1.0f, 1.0f
+    };
+
+    static unsigned int LoadTexture(char const* path)
+    {
+        unsigned int textureID;
+        glGenTextures(1, &textureID);
+
+        int width, height, channels;
+        unsigned char* data = stbi_load(path, &width, &height, &channels, 0);
+        if (data)
+        {
+            GLenum format;
+            if (channels == 1)
+                format = GL_RED;
+            else if (channels == 3)
+                format = GL_RGB;
+            else if (channels == 4)
+                format = GL_RGBA;
+
+            glBindTexture(GL_TEXTURE_2D, textureID);
+            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+            stbi_image_free(data);
+        }
+        else
+        {
+            LK_CORE_ERROR_TAG("OpenGLRenderer2D", "Texture failed to load at path {}", path);
+            stbi_image_free(data);
+        }
+        return textureID;
+    }
+
+    void GenerateCubeVaoAndVbo(unsigned int& vao, unsigned int& vbo);
+    void GeneratePlaneVaoAndVbo(unsigned int& vao, unsigned int& vbo);
+    void GenerateScreenQuadVaoAndVbo(unsigned int& vao, unsigned int& vbo);
+
+    void RenderMirrorTexture(const glm::mat4& view = glm::mat4(1.0f), const glm::mat4& proj = glm::mat4(1.0f));
+    void RenderScreenTexture(const glm::mat4& view = glm::mat4(1.0f), const glm::mat4& proj = glm::mat4(1.0f));
+    
+    Ref<Shader> GetDebugShader();
+    Ref<Shader> GetScreenShader();
+
 }
 
-constexpr int LK_RENDERER_API_MAJOR_VERSION = LkEngine::OpenGL_Major_Version;
-constexpr int LK_RENDERER_API_MINOR_VERSION = LkEngine::OpenGL_Minor_Version;
-constexpr const char* LK_SHADER_VERSION = LkEngine::OpenGL_GLSL_45;
+constexpr int LK_RENDERER_API_OPENGL_MAJOR_VERSION = LkEngine::OpenGL_Major_Version;
+constexpr int LK_RENDERER_API_OPENGL_VERSION = LkEngine::OpenGL_Minor_Version;
+constexpr const char* LK_RENDERER_API_OPENGL_SHADER_VERSION = LkEngine::OpenGL_GLSL_45;
 
-
-#endif // LK_RENDERER_API_OPENGL
