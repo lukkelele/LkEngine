190ef144cff65456446368f708711fb2f26424a2
lukkelele <{ID}+{username}@users.noreply.github.com>
2024-02-13 22:05:15 +0100

    Updated premake5 conf

--- premake5.lua
+++ premake5.lua
@@ -1,16 +1,12 @@
-include "lib/dependencies.lua"
-
 workspace "LkEngine"
     architecture "x86_64"
     startproject "LkApplication"
 
-    configurations {
-        "Debug",
-		"Release",
-        "Dist"
-    }
+    configurations { "Debug", "Release", "Dist" }
 
-    flags { "MultiProcessorCompile" }
+    flags { 
+        "MultiProcessorCompile" 
+    }
 
     filter "configurations:Debug"
         defines { "LK_DEBUG" }
@@ -27,23 +23,18 @@ workspace "LkEngine"
         runtime "Release"
         optimize "On"
 
-
+        
 outputdir = "%{cfg.buildcfg}-%{cfg.system}"
 
-include "LkApplication/premake5.lua"
-include "lib/glfw/premake5.lua"
-include "lib/glad/premake5.lua"
-include "lib/imgui/premake5.lua"
-include "lib/ImGuizmo/premake5.lua"
-include "lib/box2d/premake5.lua"
-include "lib/imgui-node-editor/premake5.lua"
+include "external/Dependencies.lua"
+
 
 project "LkEngine"
     location "LkEngine"
     kind "StaticLib"
     language "C++"
-    cppdialect "C++17"
-    staticruntime "off"
+    cppdialect "C++20"
+    staticruntime "Off"
 
     targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
     objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
@@ -52,62 +43,69 @@ project "LkEngine"
     pchsource "LkEngine/src/LKpch.cpp"
 
     defines {
-        "_GLM_WIN32",
-        "_CRT_SECURE_NO_WARNINGS",
-        "GLFW_INCLUDE_NONE",
-        "IMGUI_IMPL_OPENGL_LOADER_GLAD",
-        "IMGUI_DEFINE_MATH_OPERATORS",
-
         "LK_RENDERER_API_OPENGL",
         "LK_PHYSICS_API_BULLET3",
         "LK_PHYSICS_API_BOX2D",
+
+        "GLFW_INCLUDE_NONE",
+        "_CRT_SECURE_NO_WARNINGS",
+		"YAML_CPP_STATIC_DEFINE",
+        "IMGUI_DEFINE_MATH_OPERATORS",
     }
 
     files { 
         "%{wks.location}/LkEngine/**.h",
         "%{wks.location}/LkEngine/**.cpp",
-        "%{wks.location}/lib/stb_image/**.h",
-        "%{wks.location}/lib/stb_image/**.cpp",
+
+        "%{wks.location}/LkEngine/src/**.h",
+        "%{wks.location}/LkEngine/src/**.cpp",
+
+        "%{wks.location}/external/stb_image/**.h",
+        "%{wks.location}/external/stb_image/**.cpp",
+
+        "%{wks.location}/external/VulkanMemoryAllocator/vk_mem_alloc.h",
+        "%{wks.location}/external/VulkanMemoryAllocator/vk_mem_alloc.cpp",
    	}
 
     libdirs {
-        "%{wks.location}/lib/GLFW/lib",
+        "%{Dependencies.Glfw.LibDir}",
+        "%{Dependencies.YamlCPP.LibDir}",
     }
 
     includedirs {
         "%{prj.name}",
+
         "%{wks.location}/LkEngine/src",
         "%{wks.location}/LkEngine/src/LkEngine",
-        "%{wks.location}/lib",
-        "%{wks.location}/lib/glm",
-        "%{wks.location}/lib/glad",
-        "%{wks.location}/lib/glad/include",
-        "%{wks.location}/lib/stb_image",
-        "%{wks.location}/lib/GLFW/include",
-        "%{wks.location}/lib/spdlog/include",
-        "%{wks.location}/lib/stb_image",
 
+        "%{wks.location}/external",
+
+        "%{Dependencies.Glfw.IncludeDir}",
+        "%{Dependencies.Glad.IncludeDir}",
         "%{Dependencies.ImGui.IncludeDir}",
+        "%{Dependencies.ImGuizmo.IncludeDir}",
+        "%{Dependencies.Assimp.IncludeDir}",
+        "%{Dependencies.Entt.IncludeDir}",
+        "%{Dependencies.StbImage.IncludeDir}",
+        "%{Dependencies.Glm.IncludeDir}",
+        "%{Dependencies.Spdlog.IncludeDir}",
+        "%{Dependencies.Box2D.IncludeDir}",
+        "%{Dependencies.YamlCPP.IncludeDir}",
+        "%{Dependencies.Choc.IncludeDir}",
+        "%{Dependencies.ImGuiNodeEditor.IncludeDir}",
 
-        "%{wks.location}/lib/imgui/backends",
-        "%{wks.location}/lib/imgui/examples",
-        "%{wks.location}/lib/ImGuizmo",
-        "%{wks.location}/lib/entt/src",
-        "%{wks.location}/lib/box2d/include",
-        "%{wks.location}/lib/yaml-cpp/include",
-        "%{wks.location}/lib/imgui-node-editor",
-        "%{wks.location}/lib/choc",
+        "${wks.location}/external/VulkanMemoryAllocator",
     }
 
     links {
         "GLFW",
-        "glad",
-        "opengl32",
         "ImGui",
         "ImGuizmo",
         "box2d",
         "imgui-node-editor",
-        --"yaml-cpp"
+        "yaml-cpp",
+        "glad", 
+        "opengl32",
     }
 
 	filter "system:windows"
@@ -115,19 +113,37 @@ project "LkEngine"
             "LK_PLATFORM_WINDOWS",
             "_IMGUI_WIN32",
             "_CRT_SECURE_NO_WARNINGS",
+            "_GLM_WIN32",
+            "VK_PROTOTYPES",
+            "VK_USE_PLATFORM_WIN32_KHR",
 		}
+        includedirs { 
+            "%{Dependencies.Vulkan.Windows.IncludeDir}" 
+        }
+        libdirs { 
+            "%{Dependencies.Vulkan.Windows.LibDir}",
+            "%{Dependencies.Assimp.Windows.LibDir}",
+        }
+        links {
+            "%{Dependencies.Vulkan.Windows.LibName}",
+            "%{Dependencies.Assimp.Windows.LibName}",
+        }
+        postbuildcommands {
+            "{COPY} %{Dependencies.Assimp.Windows.LibDir}/%{Dependencies.Assimp.Windows.LibName}.dll %{cfg.targetdir}"
+        }
 
 	filter "configurations:Debug"
 		defines "LK_DEBUG"
 		runtime "Debug"
-		symbols "on"
+		symbols "On"
 
 	filter "configurations:Release"
 		defines "LK_RELEASE"
 		runtime "Release"
-		optimize "on"
+		optimize "On"
 
 	filter "configurations:Dist"
 		defines "LK_DIST"
 		runtime "Release"
-		optimize "on"
+		optimize "On"
+
