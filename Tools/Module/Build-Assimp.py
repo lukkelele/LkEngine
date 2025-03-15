97763a14cbd67dafcdf0075369d1c185ad70911f
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-03-15 13:26:14 +0100

    Add verbose and clean flags to Build-Assimp

--- Tools/Module/Build-Assimp.py
+++ Tools/Module/Build-Assimp.py
@@ -12,6 +12,14 @@ import platform
 import subprocess
 import shutil
 from sys import exit
+from pathlib import Path
+
+verbose = False
+clean = True
+
+IsPlatformActionRunner = (os.environ.get("CI") == "true")
+if IsPlatformActionRunner:
+    verbose = True
 
 import ScriptUtils as Utils
 
@@ -19,11 +27,20 @@ from ScriptUtils import ScriptLogger
 Logger = ScriptLogger("LkEngine")
 
 # Paths to Assimp, build and output directories.
-AssimpDir = os.path.join("..", "..", "External", "Assimp", "assimp")
-BuildDir = os.path.join(AssimpDir, "build")
-OutputDir = os.path.join("..", "..", "External", "Libraries")
+if Path.is_dir(Path("../../LkEngine/External")):
+    AssimpDir = os.path.join("..", "..", "LkEngine", "External", "Assimp", "assimp")
+    BuildDir = os.path.join(AssimpDir, "build")
+    OutputDir = os.path.join("..", "..", "LkEngine", "External", "Libraries")
+elif Path.is_dir(Path("../LkEngine/External")):
+    AssimpDir = os.path.join("..", "LkEngine", "External", "Assimp", "assimp")
+    BuildDir = os.path.join(AssimpDir, "build")
+    OutputDir = os.path.join("..", "LkEngine", "External", "Libraries")
+else:
+    print("Failed find assimp directory (LkEngine/External)");
+    exit(1)
 
 # Ensure output directory exists.
+if verbose: print(f"[Build-Assimp] Output dir: {OutputDir}", flush=True)
 os.makedirs(OutputDir, exist_ok=True)
 
 SystemPlatform = platform.system()
@@ -36,6 +53,7 @@ except Exception as e:
     print(f"[Build-Assimp] Error occured when trying to determine system architecture, error: {e}")
     exit(1)
 
+if verbose: print(f"[Build-Assimp] Architecture: {Architecture}")
 if Architecture != "x64":
     print(f"[Build-Assimp] Warning: System architecture is not 64-bit")
 
@@ -44,11 +62,13 @@ if SystemPlatform == "Windows":
     LibraryFileDebug = "assimp-vc143-mtd" + LibraryExtension
     LibraryFileRelease = "assimp-vc143-mt" + LibraryExtension
 elif SystemPlatform == "Linux":
-    LibraryFileDebug = "libassimp.a"
-    LibraryFileRelease = "libassimp.a" # FIXME
+    LibraryFileDebug = "libassimp" + LibraryExtension
+    LibraryFileRelease = "libassimp" + LibraryExtension
 else:
     print(f"[Build-Assimp] Unsupported platform")
     exit(1)
+
+if verbose: print(f"[Build-Assimp] Build Dir: {Path(BuildDir).resolve()}", flush=True)
 
 # TODO: Automatic selection of Debug/Release.
 BuildAsDebug = True
@@ -68,21 +88,26 @@ CMakeFlags = [
     "-DASSIMP_BUILD_FBX_EXPORTER=ON",
     "-DASSIMP_BUILD_GLTF_IMPORTER=ON",
     "-DASSIMP_BUILD_GLTF_EXPORTER=ON",
+    "-DASSIMP_BUILD_OBJ_IMPORTER=ON", 
+    "-DASSIMP_BUILD_OBJ_EXPORTER=ON",
 ]
-
 
 def _RunCMakeConfiguration():
     """Run CMake configure command."""
     # TODO: Automatic assignment of architecture.
     result = None
+    if verbose: print(f"[Build-Assimp] Current Directory: {Path(__file__).parent.resolve()}", flush=True)
+    if verbose: print(f"[Build-Assimp] Current Working Directory: {Path().resolve()}", flush=True)
     if IsPlatformWindows:
-        result = subprocess.check_call(["cmake", "..", f"-A {Architecture}"] + CMakeFlags, cwd=BuildDir)
+        #result = subprocess.check_call(["cmake", "..", f"-A {Architecture}"] + CMakeFlags, cwd=BuildDir)
+        result = subprocess.check_call(["cmake", "..", "-A", f"{Architecture}"] + CMakeFlags, cwd=BuildDir)
     else:
         result = subprocess.check_call(["cmake", ".."] + CMakeFlags, cwd=BuildDir)
     return result
 
 def _BuildAssimp():
     """Run CMake build command."""
+    if verbose: print(f"[Build-Assimp] Running: 'cmake --build . --config {BuildConfig}'", flush=True)
     build_command = ["cmake", "--build", ".", "--config", BuildConfig]
     result = subprocess.check_call(build_command, cwd=BuildDir)
     return result
@@ -96,16 +121,23 @@ def _CopyGeneratedFiles():
         SourceFile = os.path.join(BuildDir, "lib", LibraryFile)
         DestinationFile = os.path.join(OutputDir, LibraryFile)
     else:
-        print(f"[Build-Assimp] Unsupported platform")
+        print(f"[Build-Assimp] Unsupported platform", flush=True)
         exit(1)
+
+    if verbose: print(f"[Build-Assimp] Copying static library\n  Source: {Path(SourceFile).resolve()}\n  Destination: {Path(DestinationFile).resolve()}\n", flush=True)
     shutil.copy2(SourceFile, DestinationFile)
 
     # Copy config.h and revision.h headers to Assimp's include directory.
+    AssimpConfigHeader = Path(AssimpDir, "include/assimp/config.h")
+    AssimpRevisionHeader = Path(AssimpDir, "include/assimp/revision.h")
+
     ConfigHeader = os.path.join(BuildDir, "include", "assimp", "config.h")
     RevisionHeader = os.path.join(BuildDir, "include", "assimp", "revision.h")
 
-    shutil.copy2(ConfigHeader, os.path.join(AssimpDir, "include", "assimp", "config.h"))
-    shutil.copy2(RevisionHeader, os.path.join(AssimpDir, "include", "assimp", "revision.h"))
+    if verbose: print(f"[Build-Assimp] Copying config header:\n  Source: {Path(ConfigHeader).resolve()}\n  Destination: {Path(AssimpConfigHeader).resolve()}\n", flush=True)
+    if verbose: print(f"[Build-Assimp] Copying revision header:\n  Source: {Path(RevisionHeader).resolve()}\n  Destination: {Path(AssimpRevisionHeader).resolve()}\n", flush=True)
+    shutil.copy2(ConfigHeader, AssimpConfigHeader)
+    shutil.copy2(RevisionHeader, AssimpRevisionHeader)
 
     # Copy generated ZLib library.
     if SystemPlatform == "Windows":
@@ -115,23 +147,34 @@ def _CopyGeneratedFiles():
         ZLibFileName = "libzlibstatic" if BuildAsDebug else "libzlibstatic"
         ZLibFile = os.path.join(BuildDir, "contrib", "zlib", ZLibFileName + LibraryExtension)
     else:
-        print(f"[Build-Assimp] Unsupported platform")
+        print(f"[Build-Assimp] Unsupported platform", flush=True)
         exit(1)
 
+    if verbose: print(f"[Build-Assimp] Copying: {ZLibFileName}{LibraryExtension} -> {OutputDir}")
     shutil.copy2(ZLibFile, os.path.join(OutputDir, ZLibFileName + LibraryExtension))
 
 def _CleanUpAfterBuild():
     """Clean up build files."""
     if os.path.exists(BuildDir):
+        if verbose: print(f"[Build-Assimp] Cleaning up build files from '{Path(BuildDir).resolve()}'", flush=True)
         shutil.rmtree(BuildDir)
+    else:
+        print(f"[Build-Assimp] Failed to clean up build directory as the dir could not be found: {Path(BuildDir).resolve()}", flush=True)
 
 def BuildAssimp():
+    if os.path.exists(BuildDir):
+        print(f"[Build-Assimp] Build directory already exists: {BuildDir}", flush=True)
+    else: 
+        print(f"[Build-Assimp] Creating build directory: {BuildDir}", flush=True)
     os.makedirs(BuildDir, exist_ok=True)
     try:
         CMakeConfigResult = _RunCMakeConfiguration()
         BuildResult = _BuildAssimp()
         _CopyGeneratedFiles()
-        _CleanUpAfterBuild()
+        if clean:
+            _CleanUpAfterBuild()
+        else:
+            if verbose: print(f"[Build-Assimp] Skipping clean after build ({BuildDir})")
         return BuildResult 
 
     except subprocess.CalledProcessError as e:
